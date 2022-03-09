dd5c835ffc64665657b246d9480127d323f923f6
Francisco <francisco@wakemedia.mx>
2022-03-09 13:58:07 -0600

    Card animation test

--- src/components/atoms/CardsClick.jsx
+++ src/components/atoms/CardsClick.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 import card1 from '../../assets/img/card1.png';
 import data from '../../infoCards.json';
 
 import { White, Black } from '../../utils/constants'
+
+const move = keyframes`
+  to {
+    top: 35px;
+    right: 20px;
+  }
+`;
+
 
 const CardContainer = styled.div`
   position: relative;
@@ -35,8 +43,8 @@ const TitleCard = styled.h3`
 
 const InfoContainer = styled.div`
   position: absolute;
-  top: 40px;
-  right: 20px;
+  top: 15px;
+  right: 40px;
   width: 550px;
   height: 550px;
   font-size: 1.5rem;
@@ -44,6 +52,10 @@ const InfoContainer = styled.div`
   border: 4px solid ${Black};
   background-color: ${White};
   z-index: 10;
+
+  animation: ${move} 1s 1;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
 `;
 const TextCard = styled.p`
   width: 80%;
@@ -56,6 +68,7 @@ const List = styled.ul`
 const ListItem = styled.li`
   
 `;
+
 
 
 const CardsClick = () => {
