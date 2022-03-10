dafb7fd622bb3382e4e1cfd39c4bcf39c40653dd
Francisco <francisco@wakemedia.mx>
2022-03-10 15:46:51 -0600

    Responsive card

--- src/components/moleculas/CardSecction.jsx
+++ src/components/moleculas/CardSecction.jsx
@@ -5,10 +5,14 @@ import {Card1, Card2, Card3, Card4,Card5, Card6} from '../atoms/Cards';
 import { CardsClick1, CardsClick2, CardsClick3, CardsClick4, CardsClick5, CardsClick6, } from '../atoms/CardsClick';
 
 const Container = styled.div`
-  display: grid;
+  /* display: grid;
   place-items: center;
   grid-template-columns: repeat(2, 1fr);
-  grid-gap: 5rem;
+  grid-gap: 5rem; */
+  display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(30em, 1fr));
+	grid-gap: 5em;
+  justify-content: center;
   padding: 5rem;
   background-color: ${PrimaryColor};
   @media (max-width: 780px) {
